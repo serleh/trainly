1a62f6024c916d6641e4fd85b4a240cfff63f3ae
Saleh Abdullahi <salehabdullahi018@gmail.com>
2025-07-02 12:13:41 +0100

    mobile fully responsive

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
+const toggleBtn = document.getElementById("toggleBtn");
+const navLinks = document.getElementById("navLinks");
 
+toggleBtn.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
